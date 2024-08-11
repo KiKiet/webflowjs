79760ad28fb5a3dd9cdcf9330f64900448c503c6
KiKiet <kietmadrid2001@gmail.com>
2024-08-11 16:38:11 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -336,6 +336,7 @@ function handleSwitchTabForSwipeRightClick(){
 document.addEventListener("DOMContentLoaded", () => {
   // Show the loader when the page starts loading
   showLoader();
+  infoTabButton.click();
   // if (!itemLoaded) {
   //   createScrollItems();
   //   showScrollBar();
